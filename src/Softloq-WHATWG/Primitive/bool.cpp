2fa3fd4aac5155118c343054be0f4205fbb736e1
Brandon Foster <Brandon.Foster4525@outlook.com>
2024-12-09 20:03:50 -0500

    Added bool-primitive class

---
+++ src/Softloq-WHATWG/Primitive/bool.cpp
@@ -0,0 +1,26 @@
+#include "Softloq-WHATWG/Primitive/bool.hpp"
+
+namespace Softloq::WHATWG
+{
+    SOFTLOQ_WHATWG_INFRA_API Bool::Bool() : value(false) {}
+    SOFTLOQ_WHATWG_INFRA_API Bool::Bool(const Bool &boolean) : value(boolean.value) {}
+    SOFTLOQ_WHATWG_INFRA_API Bool::Bool(const bool value) : value(value) {}
+    SOFTLOQ_WHATWG_INFRA_API Bool::Bool(Bool &&boolean) : value(std::move(boolean.value)) {}
+    SOFTLOQ_WHATWG_INFRA_API Bool &Bool::operator=(const Bool &boolean)
+    {
+        Bool::value = boolean.value;
+        return *this;
+    }
+    SOFTLOQ_WHATWG_INFRA_API Bool &Bool::operator=(const bool value)
+    {
+        Bool::value = value;
+        return *this;
+    }
+    SOFTLOQ_WHATWG_INFRA_API Bool &Bool::operator=(Bool &&boolean)
+    {
+        Bool::value = std::move(boolean.value);
+        return *this;
+    }
+
+    SOFTLOQ_WHATWG_INFRA_API std::ostream &operator<<(std::ostream &out, const Bool boolean) { return out << (boolean ? "true" : "false"); }
+}
