230288e0b5e9df90669a3c932bfb903e4c467cba
Brandon Foster <Brandon.Foster4525@outlook.com>
2024-12-10 06:29:34 -0500

    Added number-primitive abstract class

--- include/Softloq-WHATWG/Primitive/number.hpp
+++ include/Softloq-WHATWG/Primitive/number.hpp
@@ -3,8 +3,17 @@
 
 #include "Softloq-WHATWG/Primitive/abstract.hpp"
 
+#include <ostream>
+
 namespace Softloq::WHATWG
 {
+    class Number : public Primitive
+    {
+    public:
+        SOFTLOQ_WHATWG_INFRA_API virtual ~Number() = 0;
+
+        SOFTLOQ_WHATWG_INFRA_API inline const Type getType() const override { return Type::Number; }
+    };
 }
 
 #endif
--- src/Softloq-WHATWG/Primitive/number.cpp
+++ src/Softloq-WHATWG/Primitive/number.cpp
@@ -1,5 +1,8 @@
 #include "Softloq-WHATWG/Primitive/number.hpp"
 
+#include <utility>
+
 namespace Softloq::WHATWG
 {
+    SOFTLOQ_WHATWG_INFRA_API Number::~Number() {}
 }
