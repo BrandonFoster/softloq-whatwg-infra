67f6bfdc08eafeb77d26a0f7f5251cf9aa05887d
Brandon Foster <Brandon.Foster4525@outlook.com>
2024-12-13 12:30:44 -0500

    implemented infra primitive base class
    Add file documentation.
    Add primitive base class.

---
+++ include/softloq/whatwg/infra/primitive/base.hpp
@@ -0,0 +1,32 @@
+/**
+ * @file base.hpp
+ * @author Brandon Foster (https://github.com/BrandonFoster)
+ * @brief Contains the WHATWG infra primitive base class.
+ * @version 1.0.0 */
+
+#ifndef SOFTLOQ_WHATWG_INFRA_PRIMITIVE_BASE_HPP
+#define SOFTLOQ_WHATWG_INFRA_PRIMITIVE_BASE_HPP
+
+#include "softloq/whatwg/infra/lib_macro.hpp"
+#include <ostream>
+
+namespace softloq::whatwg::infra
+{
+/** @brief WHATWG infra primitive types */
+enum class primitive_type { null, boolean, number, byte, byte_sequence, code_point, string, time };
+/** @brief WHATWG infra primitive base class */
+class primitive_base
+{
+public:
+    /** @brief gets the primitive type of the class. */
+    SOFTLOQ_WHATWG_INFRA_API virtual const primitive_type primitive_type() const noexcept = 0;
+    /** @brief enables the output stream operator << on the primitive class. */
+    SOFTLOQ_WHATWG_INFRA_API inline friend std::ostream& operator<<(std::ostream& out, const primitive_base& base) noexcept { base.print(out); return out; }
+    /**
+     * @brief prints the WHATWG infra representation of the primitive type to the outstream.
+     * @param out the outstream that will be used. */
+    SOFTLOQ_WHATWG_INFRA_API virtual void print(std::ostream& out) const noexcept = 0;
+};
+}
+
+#endif
