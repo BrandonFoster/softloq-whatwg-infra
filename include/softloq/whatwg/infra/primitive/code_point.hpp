2f468fd5def792de37cc39a299f4e02e6a28dbd2
Brandon Foster <Brandon.Foster4525@outlook.com>
2025-01-03 03:24:03 -0500

    add thread safe behavior to infra code point and documentation

--- include/softloq/whatwg/infra/primitive/code_point.hpp
+++ include/softloq/whatwg/infra/primitive/code_point.hpp
@@ -8,52 +8,83 @@
 #define SOFTLOQ_WHATWG_INFRA_PRIMITIVE_CODE_POINT_HPP
 
 #include "softloq/whatwg/infra/primitive/uint.hpp"
+#ifdef SOFTLOQ_MULTITHREADING
+#include <mutex>
+#endif
 
 namespace softloq::whatwg
 {
-/** @brief WHATWG infra code unit primitive class */
+/** @brief WHATWG infra code point primitive class (https://infra.spec.whatwg.org/#code-points). Thread-safe support when SOFTLOQ_MULTITHREADING is enabled. */
 class infra_code_point: public infra_primitive_base
 {
 public:
-    // WHATWG primitive base overrides //
+    // constructors //
 
-    SOFTLOQ_WHATWG_INFRA_API const infra_primitive_type primitive_type() const noexcept override;
-    SOFTLOQ_WHATWG_INFRA_API void print(std::ostream& out) const noexcept override;
-
-    //---------------------------------//
-
-    // Constructors //
-
+    /** @brief Constructs a WHATWG infra code point with a default value of U+0000. */
     SOFTLOQ_WHATWG_INFRA_API infra_code_point() noexcept;
+    /** 
+     * @brief Constructs a WHATWG infra code point with a 32-bit unsigned integer value.
+     * @param value The initial value of the code point. */
     SOFTLOQ_WHATWG_INFRA_API infra_code_point(const std::uint32_t value) noexcept;
+    /** 
+     * @brief Constructs a WHATWG infra code point with a infra 32-bit unsigned integer value.
+     * @param value The initial value of the code point that will be copied. */
     SOFTLOQ_WHATWG_INFRA_API infra_code_point(const infra_uint32& value) noexcept;
+    /** 
+     * @brief Constructs a WHATWG infra code point with a infra 32-bit unsigned integer value.
+     * @param value The initial value of the code point that will be moved. */
     SOFTLOQ_WHATWG_INFRA_API infra_code_point(infra_uint32&& value) noexcept;
+    /** 
+     * @brief Constructs a WHATWG infra code point from another infra code point.
+     * @param value The infra code point that will be copied. */
     SOFTLOQ_WHATWG_INFRA_API infra_code_point(const infra_code_point& src) noexcept;
+    /** 
+     * @brief Constructs a WHATWG infra code point from another infra code point.
+     * @param value The infra code point that will be moved. */
     SOFTLOQ_WHATWG_INFRA_API infra_code_point(infra_code_point&& src) noexcept;
     SOFTLOQ_WHATWG_INFRA_API ~infra_code_point() noexcept;
 
     //--------------//
 
-    // Assignments //
+    // assignments //
 
+    /** 
+     * @brief Assigns the WHATWG infra code point with a 32-bit unsigned integer value.
+     * @param value The initial value of the code point. */
     SOFTLOQ_WHATWG_INFRA_API infra_code_point& operator=(const std::uint32_t value) noexcept;
+    /** 
+     * @brief Assigns the WHATWG infra code point with a infra 32-bit unsigned integer value.
+     * @param value The initial value of the code point that will be copied. */
     SOFTLOQ_WHATWG_INFRA_API infra_code_point& operator=(const infra_uint32& value) noexcept;
+    /** 
+     * @brief Assigns the WHATWG infra code point with a infra 32-bit unsigned integer value.
+     * @param value The initial value of the code point that will be moved. */
     SOFTLOQ_WHATWG_INFRA_API infra_code_point& operator=(infra_uint32&& value) noexcept;
+    /** 
+     * @brief Assigns the WHATWG infra code point from another infra code point.
+     * @param value The infra code point that will be copied. */
     SOFTLOQ_WHATWG_INFRA_API infra_code_point& operator=(const infra_code_point& src) noexcept;
+    /** 
+     * @brief Assigns the WHATWG infra code point from another infra code point.
+     * @param value The infra code point that will be moved. */
     SOFTLOQ_WHATWG_INFRA_API infra_code_point& operator=(infra_code_point&& src) noexcept;
 
     //-------------//
 
-    // Conversions //
+    // conversions //
 
+    /** @brief Converts the WHATWG infra code point to a C++ 32-bit unsigned integer value. */
     SOFTLOQ_WHATWG_INFRA_API operator std::uint32_t() const noexcept;
+    /** @brief Converts the WHATWG infra code point to a infra 32-bit unsigned integer value. */
     SOFTLOQ_WHATWG_INFRA_API operator infra_uint32() const noexcept;
 
     //-------------//
 
-    // Member functions //
+    // WHATWG code point member functions //
     
+    /** @brief The C++ string representation of the WHATWG infra code point. Ex: U+0000 */
     SOFTLOQ_WHATWG_INFRA_API std::string code_point_str() const noexcept;
+    /** @brief The C++ string hex representation of the WHATWG infra code point. Ex: 0x0000 */
     SOFTLOQ_WHATWG_INFRA_API std::string hex_str() const noexcept;
 
     SOFTLOQ_WHATWG_INFRA_API const bool is_valid() const noexcept;
@@ -77,10 +108,29 @@ public:
     SOFTLOQ_WHATWG_INFRA_API const bool is_alpha() const noexcept;
     SOFTLOQ_WHATWG_INFRA_API const bool is_alnum() const noexcept;
 
-    //------------------//
+    //------------------------------------//
 
+    // WHATWG primitive base overrides //
+
+    /** @brief Returns the primitive type of the class: infra_primitive_type::infra_code_point. */
+    SOFTLOQ_WHATWG_INFRA_API const infra_primitive_type primitive_type() const noexcept override;
+    /**
+     * @brief Default ostream representation of the WHATWG infra code point primitive.
+     * @param out The ostream instance. */
+    SOFTLOQ_WHATWG_INFRA_API void print(std::ostream& out) const noexcept override;
+
+    //---------------------------------//
 private:
     infra_uint32 value;
+
+    #ifdef SOFTLOQ_MULTITHREADING
+    mutable std::mutex mtx;
+    
+    // used for threadsafe copy construction
+    SOFTLOQ_WHATWG_INFRA_API infra_code_point(const infra_code_point& src, const std::lock_guard<std::mutex>&) noexcept;
+    // used for threadsafe move construction
+    SOFTLOQ_WHATWG_INFRA_API infra_code_point(infra_code_point&& src, const std::lock_guard<std::mutex>&) noexcept;
+    #endif
 };
 }
 
