56adfd2171247d6bf4799fb8cfd4400c95c4b695
Brandon Foster <Brandon.Foster4525@outlook.com>
2025-01-03 01:37:13 -0500

    add thread safe behavior to infra null and documentation

--- include/softloq/whatwg/infra/primitive/null.hpp
+++ include/softloq/whatwg/infra/primitive/null.hpp
@@ -11,16 +11,20 @@
 
 namespace softloq::whatwg
 {
-/** @brief WHATWG infra null primitive class */
+/** @brief WHATWG infra null primitive class (https://infra.spec.whatwg.org/#nulls). Thread-safe support when SOFTLOQ_MULTITHREADING is enabled. */
 class infra_null: public infra_primitive_base
 {
 public:
     // WHATWG primitive base overrides //
 
+    /** @brief Returns the primitive type of the class: infra_primitive_type::infra_null. */
     SOFTLOQ_WHATWG_INFRA_API const infra_primitive_type primitive_type() const noexcept override;
+    /**
+     * @brief Default ostream representation of the WHATWG infra null primitive.
+     * @param out The ostream instance. */
     SOFTLOQ_WHATWG_INFRA_API void print(std::ostream& out) const noexcept override;
 
-    //---------------------------------//
+    //---------------------------------// 
 };
 }
 
